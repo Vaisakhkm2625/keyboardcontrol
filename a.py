6795d8e2f48b1cc6f71c615fc72d32953a8ee837
Vaisakh K M <vaisakhkm2625@gmail.com>
2023-10-18 00:48:12 +0530

    added pyyaml and testing file a.py

---
+++ a.py
@@ -0,0 +1,43 @@
+import yaml
+
+_xremap_config = { 'modmap': [], 'keymap': [] }
+
+def define_modmap(modmap):
+    global _xremap_config
+    _xremap_config['modmap'].append({
+        'name': 'xremap-python',
+        'remap': modmap,
+    })
+
+def define_conditional_modmap(application, modmap):
+    global _xremap_config
+    _xremap_config['modmap'].append({
+        'name': 'xremap-python',
+        'remap': modmap,
+        'application': application,
+    })
+
+def define_keymap(application, keymap, name):
+    global _xremap_config
+    entry = {
+        'name': 'xremap-python',
+        'remap': keymap,
+    }
+    if application:
+        entry['application'] = application
+    _xremap_config['keymap'].append(entry)
+
+def xremap_config():
+    global _xremap_config
+    return _xremap_config
+
+
+define_keymap("chrome","capslock","cat")
+print(_xremap_config)
+
+
+print("------")
+print(yaml.dump(_xremap_config))
+
+
+
