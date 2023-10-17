f87b2faef8da2f517cd453de90ad04d13ffc938f
Vaisakh K M <vaisakhkm2625@gmail.com>
2023-10-18 03:31:22 +0530

    qt dev envirnment added

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,7 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [ pkgs.git pkgs.qt6.full pkgs.python310Packages.pyqt6  ];
 
   # https://devenv.sh/scripts/
   scripts.hello.exec = "echo hello from $GREET";
